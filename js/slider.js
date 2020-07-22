4f57594118eb3a032fec4e9a7aea32911b85445d
sk-portfolio <catalonya1111@gmail.com>
2020-07-22 22:32:37 +0300

    ,j

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.slider_1').slick({
+  	infinite: true,
+  	slidesToShow: 3,
+  	slidesToScroll: 1,
+  	arrows:true,
+ 	dots:false
+  });
+});
---
+++ js/slider_1.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.slider_1').slick({
+  	infinite: true,
+  	arrows:false,
+ 	dots:true
+  });
+});
+$(document).ready(function(){
+  $('.slider_2').slick({
+  	infinite: true,
+  	arrows:true,
+ 	dots:false
+  });
+});
